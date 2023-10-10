d5621f8adc22c42b811aab10c3b5e2e6c7dbda2b
saikumarmarripally <saikumarmarripally38@gmail.com>
2023-10-10 07:20:08 +0000

    commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className="background">
+    <div>
+      <h1 className="heading">Super Over Leage</h1>
+    </div>
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="img"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="img"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
